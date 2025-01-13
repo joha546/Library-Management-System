6bfd5e7b951927c85a78ab1f155333663b048fd5
Md Khaled Bin Joha <joh60483@gmail.com>
2025-01-14 00:59:15 +0600

    Database updated

---
+++ Library/LibraryData/LibraryContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patron {  get; set; }
+
+    }
+}
---
+++ Library/LibraryData/Models/Patron.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        // public virtual LibraryCard LibraryCard { get; set; }
+
+    }
+}
